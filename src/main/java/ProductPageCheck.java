67ed4759e101de47d624f3e724734f8c6c9b5d70
Efratov Pavel <pavelefratov@gmail.com>
2024-02-12 23:31:19 +0300

    Задание 10. Проверить, что открывается правильная страница товара (Часть 2)

--- src/main/java/ProductPageCheck.java
+++ src/main/java/ProductPageCheck.java
@@ -40,23 +40,32 @@ public class ProductPageCheck {
         String campaignPriceColor = element.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
         Dimension campaignPriceTextSize = element.findElement(By.cssSelector("strong.campaign-price")).getSize();
 
-        boolean isCampaignStronger = false;
-        if (regularPriceTextSize.getWidth() < campaignPriceTextSize.getWidth() &&
-                regularPriceTextSize.getHeight() < campaignPriceTextSize.getHeight()) {
-            isCampaignStronger = true;
-        }
+        element.click();
 
-        System.out.println(title + " \n"
-                + regularPrice + "\n"
-                + checkColor(regularPriceColor) + " " + regularPriceTag + "\n"
-                + campaignPrice + "\n"
-                + checkColor(campaignPriceColor) + " " + campaignPriceTag + "\n"
-                + regularPriceTextSize + " "
-                + campaignPriceTextSize + " " + isCampaignStronger);
+        System.out.println("Проверка страницы товара.");
+        String productTitle = driver.findElement(By.tagName("h1")).getText();
+        String productRegularPrice = driver.findElement(By.cssSelector("s.regular-price")).getText();
+        String productRegularPriceTag = driver.findElement(By.cssSelector("s.regular-price")).getTagName();
+        String productRegularPriceColor = driver.findElement(By.cssSelector("s.regular-price")).getCssValue("color");
+        String productCampaignPrice = driver.findElement(By.cssSelector("strong.campaign-price")).getAttribute("textContent");
+        String productCampaignPriceTag = driver.findElement(By.cssSelector("strong.campaign-price")).getTagName();
+        String productCampaignPriceColor = driver.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
+        Dimension productRegularPriceSize = driver.findElement(By.cssSelector("s")).getSize();
+        Dimension productCampaignPriceSize = driver.findElement(By.cssSelector("strong.campaign-price")).getSize();
 
+        System.out.println("а) Совпадает название товара: " + (title.equals(productTitle)));
+        System.out.println("б) Совпадают цены: обычная - " + (regularPrice.equals(productRegularPrice))
+                + ", акционная - " + (campaignPrice.equals(productCampaignPrice)));
+        System.out.println("в) Обычная цена зачеркнута и помечена серым цветом.\nГлавная страница: цвет - " + checkColor(regularPriceColor)
+                + ", тэг элемента : " + regularPriceTag
+                + "\nСтраница товара: цвет - " + checkColor(productRegularPriceColor) + ", тэг элемента : " + productRegularPriceTag);
+        System.out.println("г) Акционная цена зачеркнута и помечена серым цветом.\nГлавная страница: цвет - " + checkColor(campaignPriceColor)
+                + ", тэг элемента : " + campaignPriceTag
+                + "\nСтраница товара: цвет - " + checkColor(productCampaignPriceColor) + ", тэг элемента : " + productCampaignPriceTag);
+        System.out.println("д) Акционная цена крупнее. \nГлавная страница: " + isCampaignBigger(regularPriceTextSize, campaignPriceTextSize)
+                + "\nСтраница продукта: " + isCampaignBigger(productRegularPriceSize, productCampaignPriceSize));
 
         driver.quit();
-
     }
 
     @After
@@ -80,6 +89,11 @@ public class ProductPageCheck {
             return "GRAY";
         }
         return null;
+    }
+
+    public boolean isCampaignBigger(Dimension regularSize, Dimension campaignSize) {
+        return regularSize.getWidth() < campaignSize.getWidth() &&
+                regularSize.getHeight() < campaignSize.getHeight();
     }
 
 
