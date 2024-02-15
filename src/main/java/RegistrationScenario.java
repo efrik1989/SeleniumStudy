3503d03d28814563787c4651b1018587a3acb763
Efratov Pavel <pavelefratov@gmail.com>
2024-02-15 23:58:41 +0300

    11 звдание - сценарий регистрации и логина нового покупателя.

---
+++ src/main/java/RegistrationScenario.java
@@ -0,0 +1,91 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.xml.datatype.DatatypeConstants;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+public class RegistrationScenario {
+
+    WebDriver driver;
+    final static long DEFAULT_TIMEOUT = 5000;
+
+    @Before
+    public void beforeTest(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void registrationCheck() throws Exception {
+        driver.get("http://localhost/litecard");
+
+        System.out.println("Регистрация нового пользователя");
+        //Регистрация нового пользователя
+        driver.findElement(By.cssSelector("#box-account-login > div > form > table > tbody > tr:nth-child(5) > td > a")).click();
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=firstname]")).sendKeys("Andrew");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=lastname]")).sendKeys("Talbot");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=address1]")).sendKeys("2nd avenue");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=postcode]")).sendKeys("55555");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=city]")).sendKeys("Providance");
+        Select select = new Select(driver.findElement(By.cssSelector("#create-account > div > form > table > tbody > tr:nth-child(5) > td:nth-child(1) select")));
+        select.selectByVisibleText("United States");
+
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=email]")).sendKeys("something@some.com");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=phone]")).sendKeys("7775555555");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=password]")).sendKeys("aaaaaa");
+        driver.findElement(By.cssSelector("#create-account > div > form input[name=confirmed_password]")).sendKeys("aaaaaa");
+
+        driver.findElement(By.cssSelector("#create-account > div button[name=create_account]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.notice.success")));
+        System.out.println("Регистрация успешна");
+
+        //логаут
+        driver.findElement(By.cssSelector("#box-account > div > ul > li:nth-child(4) > a")).click();
+        System.out.println("Логаут");
+
+        //логин под только что созданным покупателем.
+        driver.findElement(By.name("email")).sendKeys("something@some.com");
+        driver.findElement(By.name("password")).sendKeys("aaaaaa");
+        driver.findElement(By.cssSelector("#box-account-login form button[name=login]")).click();
+        System.out.println("Логин");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.notice.success")));
+
+        //логаут
+        driver.findElement(By.cssSelector("#box-account > div > ul > li:nth-child(4) > a")).click();
+        System.out.println("Логаут");
+        driver.quit();
+
+    }
+
+    @After
+    public void afterTest() {
+        try {
+            driver.close();
+        }catch (Exception e) {
+            System.out.println("Browser is already closed.");
+        }
+
+    }
+
+}
+
