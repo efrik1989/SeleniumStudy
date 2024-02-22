c95e48f498e5b544fee715dc521241e29e2c4bf1
Efratov Pavel <pavelefratov@gmail.com>
2024-02-22 18:58:34 +0300

    Задание 13. Сделайте сценарий работы с корзиной Правки по замечаниям.

--- src/main/java/CartWorkCheckScenario.java
+++ src/main/java/CartWorkCheckScenario.java
@@ -3,17 +3,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,10 +28,10 @@ public class CartWorkCheckScenario {
     @Before
     public void beforeTest(){
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        //ChromeOptions options = new ChromeOptions();
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(8));
 
     }
 
@@ -41,34 +45,55 @@ public class CartWorkCheckScenario {
             System.out.println("Открыть первый товар из списка");
             driver.findElement(By.cssSelector("div.content li.product")).click();
             //Проверить коичество товара в корзине
-            int productCountBefore = Integer.parseInt(driver.findElement(By.cssSelector("div#cart span.quantity"))
+            int productCountBefore = Integer.parseInt(wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.cssSelector("div#cart span.quantity")))
                     .getAttribute("textContent"));
 
             System.out.println("Добавить товар в корзину");
-            if (!wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("#box-product select")))) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
+            if (driver.findElements(By.cssSelector("#box-product select")).size() > 0) {
                 Select select = new Select(driver.findElement(By.cssSelector("#box-product select")));
                 select.selectByVisibleText("Small");
             }
             driver.findElement(By.cssSelector("button[name^=add]")).click();
 
-            int productCountAfter = Integer.parseInt(driver.findElement(By.cssSelector("div#cart span.quantity"))
-                    .getAttribute("textContent"));
+            int productCountAfter = 0;
+            boolean isProductCountChanged = wait.until(ExpectedConditions
+                    .attributeContains(By.cssSelector("div#cart span.quantity"),"textContent", "" + (productCountBefore+1)));
+            if (isProductCountChanged) {
+                productCountAfter = Integer.parseInt(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#cart span.quantity")))
+                        .getAttribute("textContent"));
+            }
 
             System.out.println("Количество товаров в корзине увеличилось: " + (productCountBefore < productCountAfter) +
-                    " = " + productCountBefore);
+                    " = " + productCountAfter);
 
             driver.findElement(By.cssSelector("#site-menu li.general-0 > a")).click();
         }
 
         driver.findElement(By.cssSelector("#cart > a.link")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h2")));
 
+        System.out.println("Удаляем товары из корзины");
         while (true) {
-            if (!wait.until(ExpectedConditions.textToBe(By.cssSelector("#checkout-cart-wrapper > p:nth-child(1) > em"), "There are no items in your cart."))) {
-                driver.findElement(By.cssSelector("#box-checkout-cart button[name^=remove]")).click();
-            } else break;
+            List<WebElement> positions = driver.findElements(By.cssSelector("#order_confirmation-wrapper > table > tbody > tr > td.item"));
+            int quantity = quantityCheck(positions);
+            if (quantity == 0) {
+                break;
+            }
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#box-checkout-cart button[name^=remove]"))).click();
+            System.out.println("Количество товаров в корзине: " + quantity);
+            try {
+                for (WebElement el : positions) {
+                    wait.until(ExpectedConditions.stalenessOf(el));
+                }
+            } catch (StaleElementReferenceException e) {
+                System.out.println("Список товаров обновился.");
+            }
         }
         System.out.println("Все товары удалены из корзины");
         driver.quit();
+
 
     }
 
@@ -80,6 +105,16 @@ public class CartWorkCheckScenario {
             System.out.println("Browser is already closed.");
         }
 
+    }
+
+    public int quantityCheck(List<WebElement> positions) {
+        int quantity = 0;
+        for (WebElement el : positions) {
+            String qua = el.findElement(By.xpath("..//td[1]")).getText();
+            quantity+= Integer.parseInt(qua);
+            System.out.println(qua + " | " + el.getText());
+        }
+        return quantity;
     }
 
 }
